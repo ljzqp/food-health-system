f5a83a37b6f19fecb6f17fe9f61b04f2b0db71b8
罗健 <2541147551@qq.com>
2025-02-27 17:18:06 +0800

    提交代码

--- backend/app.py
+++ backend/app.py
@@ -12,25 +12,11 @@ def register():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-    name = data.get('name')
-    gender = data.get('gender')
-    height = data.get('height')
-    weight = data.get('weight')
-    dietType = data.get('dietType')
-
+    
     if username in users:
         return jsonify({'message': '用户已存在'}), 400
-
-    # 保存用户信息
-    users[username] = {
-        'password': password,
-        'name': name,
-        'gender': gender,
-        'height': height,
-        'weight': weight,
-        'dietType': dietType
-    }
-
+    
+    users[username] = password
     return jsonify({'message': '注册成功'}), 201
 
 @app.route('/api/login', methods=['POST'])
@@ -38,12 +24,10 @@ def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-
-    user = users.get(username)
-    if user and user['password'] == password:
+    
+    if username in users and users[username] == password:
         return jsonify({'message': '登录成功'}), 200
-    else:
-        return jsonify({'message': '用户名或密码错误'}), 401
+    return jsonify({'message': '用户名或密码错误'}), 401
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
